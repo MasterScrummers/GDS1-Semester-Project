66c27080dffbfc2a6ea2d2075ef152ecf74cf3f5
Ayush <kushal.a.bilash@student.uts.edu.au>
2022-04-01 20:55:37 +1100

    Horizontal Player Movement Added
    "As a player, I want to be able to move left and right so that I can navigate the game level"
    
    Player can now move left and right

--- Assets/Scripts/Player/PlayerInput.cs
+++ Assets/Scripts/Player/PlayerInput.cs
@@ -5,8 +5,10 @@ public class PlayerInput : MonoBehaviour
     private InputController ic; //InputController
     public float cooldownTimer = 10f; //The cooldown timer
     private float currCooldownTimer; //Current cooldown tick
+    private Rigidbody2D rb; //Kirby Rigidbody2D
+    private Vector2 vel; //Kirby velocity 
 
-    public float speed = 1f; //Speed of the character
+    public float speed = 5f; //Speed of the character
 
     [HideInInspector] public WeaponBase lightWeapon; //The assigned light weapon
     [HideInInspector] public WeaponBase heavyWeapon; //The assigned heavy weapon
@@ -16,6 +18,8 @@ public class PlayerInput : MonoBehaviour
     {
         ic = DoStatic.GetGameController().GetComponent<InputController>();
         currCooldownTimer = cooldownTimer;
+        rb = GetComponent<Rigidbody2D>();
+        vel = rb.velocity;
     }
 
     void Update()
@@ -25,10 +29,26 @@ public class PlayerInput : MonoBehaviour
          * Probably use Rigidbody2D for movement.
          * 
          * Vector3 var = rb.velocity;
-         * variable.x *= speed * ic.ic.axisRawValues["Horizontal"];
+         * variable.x *= speed * ic.axisRawValues["Horizontal"];
          * 
          * Use ic.buttonDowns["Jump"] for the jump.
          */
+
+
+        if (ic.axisRawValues["Horizontal"] != 0)
+        {
+            vel = new Vector2(speed * ic.axisRawValues["Horizontal"], rb.velocity.y);
+
+            rb.velocity = vel;
+        }
+        else
+        {
+            vel = new Vector2(0, rb.velocity.y);
+            rb.velocity = vel;
+        }
+
+            
+
 
         if (ic.buttonDowns["Light"] && lightWeapon)
         {
