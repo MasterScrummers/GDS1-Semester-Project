06036c6b654f692cdf22569c78bc977a39956579
NovGS <70025949+NovGS@users.noreply.github.com>
2022-04-12 21:10:57 +1000

    Hammer Light Attack Animation & HitBox

---
+++ Assets/Scripts/Weapons/Hammer.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class Hammer : WeaponBase
+{
+    // Start is called before the first frame update
+    public Hammer() : base()
+    {
+        weaponName = "Hammer";
+        description = "Hammer";
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
