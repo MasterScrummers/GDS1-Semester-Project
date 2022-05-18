1ec10218399a18cd0df3523f596dc6ea81ed60cc
NovGS <70025949+NovGS@users.noreply.github.com>
2022-05-18 23:05:50 +1000

    fine-tuned Hammer Animation & Knockback

--- Assets/Scripts/Player/PlayerInput.cs
+++ Assets/Scripts/Player/PlayerInput.cs
@@ -48,9 +48,9 @@ public class PlayerInput : MonoBehaviour
         orignalspeed = speed;
         originalGravityMultiplier = gravityMultiplier;
 
-        lightWeapon = new Sword();
-        heavyWeapon = new Sword();
-        specialWeapon = new Sword();
+        lightWeapon = new Hammer();
+        heavyWeapon = new Hammer();
+        specialWeapon = new Hammer();
     }
 
     void Update()
